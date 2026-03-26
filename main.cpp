770558372be8aa37e8a76878b41a90e5233db9a1
Michael <michael.short094@gmail.com>
2026-03-25 22:42:05 -0600

    smoothing applied

--- main.cpp
+++ main.cpp
@@ -23,19 +23,39 @@ std::vector<double> smooth_data(fs::path file_path) {
   }
 
   //create a vector to return and needed vars
-  std::vector<double> storage_vector;
+  std::vector<double> rough_vector;
   std::fstream file_stream(file_path);
   std::string line;
   // First four lines go in as is
-  for (auto i=0; i<=4; i++) {
-    std::getline(file_stream, line);
-    storage_vector.push_back(std::stod(line));
-  }
   while (std::getline(file_stream, line)){
-  //TODO: fill out the smoothing algorithim  
+    rough_vector.push_back(std::stod(line) * -1);
   }
-  // RVO should do this but i want to be explicit
-  return std::move(storage_vector);
+  std::vector<double> smooth_vector(rough_vector.size());
+  //Add first four from rough
+  for (int i=0; i < 3; i++) {
+    smooth_vector[i] = (rough_vector[i]);
+  }
+  //apply weighted average to middle
+  for (int i=3; i < rough_vector.size() -3; i++) {
+    smooth_vector[i] = ((rough_vector[i - 3] +
+    2*rough_vector[i - 2] +
+    3*rough_vector[i - 1] +
+    3*rough_vector[i] +
+    3*rough_vector[i + 1] +
+    2*rough_vector[i + 2] +
+    rough_vector[i + 3]) / 15);
+  }
+  //Add last three to smoothed vector
+  for (int i=rough_vector.size() - 3; i < rough_vector.size(); i++) {
+    smooth_vector[i] = (rough_vector[i]);
+  }
+  // RVO should move to avoid an unnesicary copy
+  //test prints:
+  for (size_t i = 0; i < rough_vector.size(); ++i) {
+    std::cout << rough_vector[i] << " \t " << smooth_vector[i] << std::endl;
+  }
+
+  return smooth_vector;
 }
 
 //testing function, MAKE SURE TO REMOVE AFTER NO LONGER NEEDED
@@ -51,7 +71,7 @@ int main() {
   fs::path dir_path = "./data_files";
   if (fs::exists(dir_path) && fs::is_directory(dir_path)){
     for (auto &file : fs::directory_iterator(dir_path)){
-      printfile(file);
+      smooth_data(file);
     }
   } else {
     std::cerr << "ERROR: directory not found at" << dir_path;
